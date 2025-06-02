29e6b568535ff76cd648e3944de4f713dc97a029
nightnoryu <qazwf734@gmail.com>
2025-06-02 13:06:25 +0300

    Implement sample handler

--- pkg/infrastructure/telegram/handler/janitorhandler.go
+++ pkg/infrastructure/telegram/handler/janitorhandler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/go-telegram/bot/models"
 	"github.com/nightnoryu/janitor/pkg/infrastructure/jsonlog"
@@ -12,8 +13,22 @@ import (
 
 func NewJanitorHandler(logger jsonlog.Logger) tg.HandlerFunc {
 	return func(ctx context.Context, bot *tg.Bot, update *models.Update) {
-		if update.Message == nil {
+		if update.Message.Chat.Type != models.ChatTypeSupergroup {
 			return
+		}
+		if !strings.HasPrefix(update.Message.Text, "/ban") {
+			return
+		}
+		if update.Message.ReplyToMessage == nil {
+			return
+		}
+
+		_, err := bot.SendMessage(ctx, &tg.SendMessageParams{
+			Text:   "ban user " + update.Message.ReplyToMessage.From.Username,
+			ChatID: update.Message.Chat.ID,
+		})
+		if err != nil {
+			logger.Error(err)
 		}
 
 		logger.Info(fmt.Sprintf(
